a78dac9130e9c810aab33ed26b418d16d471f154
Vladimir Sedikh <andiwillfly@dev.casino-x.com>
2018-01-17 14:01:11 +0200

    Add artists route

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const Express = require('express');
-
+let request = require('request');
+request = request.defaults({ jar: true });
 
 // API KEY: fb9d42de15720bcb20e6ed6fc5016a4c
 const app = Express();
@@ -13,7 +14,13 @@ app.use('/static', Express.static(path.join(__dirname, './static')));
 
 
 
-app.use('/interface_players', (req, res)=> interfacePlayersRoute(DB, req, res) );
+app.use('/artists', (req, res)=> {
+	request.post({
+		url: 'http://ws.audioscrobbler.com/2.0/?method=library.getartists&api_key=fb9d42de15720bcb20e6ed6fc5016a4c&user=joanofarctan&format=json'
+	}, function(error, response, body) {
+		res.send(body);
+	});
+});
 
 
 app.get('*', (req, res)=> {
