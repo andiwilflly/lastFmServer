fe8f3962a3102b78f170845eaf2a30ea38ac19ab
Vladimir Sedikh <andiwillfly@dev.casino-x.com>
2018-01-17 14:03:53 +0200

    Add artists route

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-const fs = require('fs');
-const path = require('path');
-const Express = require('express');
-let request = require('request');
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var Express = require('express');
+var request = require('request');
 request = request.defaults({ jar: true });
 
 // API KEY: fb9d42de15720bcb20e6ed6fc5016a4c
-const app = Express();
+var app = Express();
 
 // Serve built files with express static files middleware
-app.use('/built', Express.static(path.join(__dirname, './built')));
+app.use('/', Express.static(path.join(__dirname, './')));
 // Serve normal requests with our handleRender function
 app.use('/static', Express.static(path.join(__dirname, './static')));
 
 
-
-app.use('/artists', (req, res)=> {
+app.use('/artists', function (req, res) {
 	request.post({
 		url: 'http://ws.audioscrobbler.com/2.0/?method=library.getartists&api_key=fb9d42de15720bcb20e6ed6fc5016a4c&user=joanofarctan&format=json'
-	}, function(error, response, body) {
+	}, function (error, response, body) {
 		res.send(body);
 	});
 });
 
 
-app.get('*', (req, res)=> {
+app.get('*', function (req, res) {
 	fs.readFile('./index.html', 'utf8', function (err, file) {
 		if (err) {
 			return console.log(err);
@@ -32,4 +33,6 @@ app.get('*', (req, res)=> {
 	});
 });
 
+
 app.listen(3000);
+
